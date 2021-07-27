550730831aa1583377c89cd9add4180f6b3703a7
Ben Hsieh <bqh5026@gmail.com>
2021-07-26 21:59:42 -0500

    Update digital currencies component. Edit homepage component.

--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -11,7 +11,7 @@ const HomePage = () => (
    feelings is to make proper investments and watch those investments grow in a diversified portfolio.
    United States of America is one of the best countries to invest in various instruments in the world. There 
    are may reasons why USA is a safe country to invest in. For instance, over the years the goverment
-   had passed regulations that made investing safer and not subject to frauds.
+   has passed regulations that made investing safer and not subject to frauds.
    So what are the options to invest in the market? The purpose of this website is to provide 
    an overview of the exciting investment world.</p>
 
