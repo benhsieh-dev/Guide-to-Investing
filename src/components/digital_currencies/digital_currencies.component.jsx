1933f59fc629e9e7cc17fc99a0540d7b1b994f1f
Ben Hsieh <bqh5026@gmail.com>
2021-07-27 22:33:14 -0500

    Add css stylings to crypto currenciessearch results

--- src/components/digital_currencies/digital_currencies.component.jsx
+++ src/components/digital_currencies/digital_currencies.component.jsx
@@ -65,7 +65,16 @@ export default () => {
                 </div>
               <div className="coin-data">
                 <p className="coin-price">${coin.current_price}</p>
-                <p className="coin-volume">${coin.market_cap.toLocaleString()}</p>
+                <p className="coin-volume">${coin.total_volume.toLocaleString()}</p>
+                {
+                  coin.price_change_percentage_24h < 0 ? 
+                  (<p className="coin-percent red">{coin.price_change_percentage_24h.toFixed(2)}%</p>) 
+                  : 
+                  ( <p className="coin-percent green">{coin.price_change_percentage_24h.toFixed(2)}%</p>)
+                }
+                <p className="coin-marketcap">
+                  Mkt Cap: ${coin.market_cap.toLocaleString()}
+                </p>
               </div>
             </div>
         </div>
