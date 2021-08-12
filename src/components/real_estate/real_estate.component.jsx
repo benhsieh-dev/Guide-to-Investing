bc6319a94b7ffb2410cf4ca1e5a9faf917455675
Ben Hsieh <bqh5026@gmail.com>
2021-08-11 23:40:10 -0500

    Test data with new endpoint

--- src/components/real_estate/real_estate.component.jsx
+++ src/components/real_estate/real_estate.component.jsx
@@ -1,35 +1,90 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import './real_estate.css'; 
 
-fetch(
-  "https://real-estate-usa.p.rapidapi.com/api/v1/properties?postal_code=94105&offset=0&limit=200",
-  {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": "b519a2b20amsh014be75ec062cdap1aaac1jsn918f03506c38",
-      "x-rapidapi-host": "real-estate-usa.p.rapidapi.com",
-    },
-  }
-)
-  .then(response => response.json())
-  .then((response) => {
-    console.log(response);
-    console.log(response.properties);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+// fetch(
+//   "https://real-estate-usa.p.rapidapi.com/api/v1/properties?postal_code=94105&offset=0&limit=200",
+//   {
+//     method: "GET",
+//     headers: {
+//       "x-rapidapi-key": "b519a2b20amsh014be75ec062cdap1aaac1jsn918f03506c38",
+//       "x-rapidapi-host": "real-estate-usa.p.rapidapi.com",
+//     },
+//   }
+// )
+//   .then(response => response.json())
+//   .then((response) => {
+//     console.log(response);
+//     console.log(response.properties);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
 
+import axios from "axios";
+
+const options = {
+  method: "GET",
+  url: "https://real-estate-usa.p.rapidapi.com/api/v1/schools",
+  params: { lat: "35.129431", lon: "-117.937021" },
+  headers: {
+    "x-rapidapi-key": "b519a2b20amsh014be75ec062cdap1aaac1jsn918f03506c38",
+    "x-rapidapi-host": "real-estate-usa.p.rapidapi.com",
+  },
+};
+
+// axios
+//   .request(options)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
 
 export default () => {
-  
+
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    // fetch(
+    //   "https://real-estate-usa.p.rapidapi.com/api/v1/properties?postal_code=94105&offset=0&limit=200",
+    //   {
+    //     method: "GET",
+    //     headers: {
+    //       "x-rapidapi-key":
+    //         "b519a2b20amsh014be75ec062cdap1aaac1jsn918f03506c38",
+    //       "x-rapidapi-host": "real-estate-usa.p.rapidapi.com",
+    //     },
+    //   }
+    // )
+    //   .then((response) => response.json())
+    //   .then((response) => {
+    //     console.log(response);
+    //     console.log(response.properties);
+    //   })
+    //   .then(response => setProperties(response.properties))
+    //   .catch((err) => {
+    //     console.error(err);
+    //   });
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .then((response) => setProperties(response.data.schools))
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, [])
+
   return (
   <div className="real_estate">
     <h1>Real Estates</h1>
     <p>Location is very important in real estate investments. From personal experience, I believe
     that New York real estate is uniquely strong due to its population density among other 
     factors. Maryland, on the other hand, offers better bargain per square foot of real estate.</p>
+    {properties}
   </div>
   )
 }
