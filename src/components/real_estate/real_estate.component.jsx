552087e025e11f10acb7b48b80faf9596b80f556
Ben Hsieh <bqh5026@gmail.com>
2021-08-27 23:58:27 -0500

    Edit comments in real_estate.component.jsx

--- src/components/real_estate/real_estate.component.jsx
+++ src/components/real_estate/real_estate.component.jsx
@@ -4,10 +4,6 @@ import React, {useEffect, useState} from 'react';
 
 import './real_estate.css'; 
 
-
-// import axios from "axios";
-
-
 // real estate api
 import axios from "axios";
 
@@ -22,7 +18,6 @@ const options = {
     type: "single-family,multi-family",
   },
   headers: {
-    // "x-rapidapi-key": "b519a2b20amsh014be75ec062cdap1aaac1jsn918f03506c38",
     "x-rapidapi-key": `${process.env.REACT_APP_REAL_ESTATE_KEY}`,
     "x-rapidapi-host": "real-estate12.p.rapidapi.com",
   },
@@ -30,21 +25,13 @@ const options = {
 
 export default () => {
   const [properties, setProperties] = useState([]);
-  // const options = {
-  //   method: "GET",
-  //   url: "https://real-estate-usa.p.rapidapi.com/api/v1/properties",
-  //   params: { postal_code: "60611", offset: "0", limit: "200" },
-  //   headers: {
-  //     "x-rapidapi-key": "b519a2b20amsh014be75ec062cdap1aaac1jsn918f03506c38",
-  //     "x-rapidapi-host": "real-estate-usa.p.rapidapi.com",
-  //   },
-  // };
 
   useEffect(() => {
     axios
       .request(options)
       .then(function (response) {
-        console.log(response.data.properties);
+        console.log(response.data);
+        // console.log(response.data.properties);
         setProperties(response.data.properties);
         // console.log(`these are the ${properties}`);
       })
@@ -52,43 +39,6 @@ export default () => {
         console.error(error);
       });
   }, [])
-
-  // useEffect(() => {
-  //   axios
-  //     .request(options)
-  //     .then((response) => {
-  //       console.log(response.data.properties);
-  //       setProperties(response.data.properties);
-  //       // console.log(`these are the ${properties}`);
-  //     })
-  //     .catch((error) => console.error(error)
-  //     );
-  // }, [options, properties]);
-
-  // useEffect(() => {
-  //   fetch(
-  //     "https://real-estate-usa.p.rapidapi.com/api/v1/properties?postal_code=60611&offset=0&limit=200",
-  //     {
-  //       method: "GET",
-    //     headers: {
-    //       "x-rapidapi-key":
-    //         // "b519a2b20amsh014be75ec062cdap1aaac1jsn918f03506c38",
-    //       `${process.env.REACT_APP_REAL_ESTATE_USA_KEY}`,
-    //       "x-rapidapi-host": "real-estate-usa.p.rapidapi.com",
-    //     },
-    //   }
-    // )
-      // .then(console.log(process.env))
-  //     .then((response) => response.json())
-  //     .then((response) => {
-  //       // console.log(response);
-  //       console.log(response.properties);
-  //       setProperties(response.properties);
-  //     })
-  //     .catch((err) => {
-  //       console.error(err);
-  //     });
-  // }, [])
 
   return (
     <div className="real_estate">
@@ -100,13 +50,6 @@ export default () => {
         hand, offers better bargain per square foot of real estate.
       </p>
       <div>
-        {/* <ul>
-          {properties === undefined ? properties.map((property, idx) => (
-            <div key={idx}>
-              <li>{property.permalink}</li>
-            </div>
-          )) : <div></div>}
-          </ul> */}
         <ul>
           {properties.map((property, idx) => (
             <div key={idx}>
