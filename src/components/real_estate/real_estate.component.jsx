29ae423ae2d91edacf51f9a8e7fb636dba885dbb
Ben Hsieh <bqh5026@gmail.com>
2023-03-27 23:12:37 -0500

    Fix overstack error

--- src/components/real_estate/real_estate.component.jsx
+++ src/components/real_estate/real_estate.component.jsx
@@ -1,44 +1,44 @@
-import React, {useEffect, useState} from 'react';
+// import React, {useEffect, useState} from 'react';
 // import React, {useEffect} from "react";
-// import React from "react";
+import React from "react";
 
 import './real_estate.css'; 
 
 // real estate api
-import axios from "axios";
+// import axios from "axios";
 
-const options = {
-  method: "GET",
-  url: "https://real-estate12.p.rapidapi.com/listings/sale",
-  params: {
-    state: "CA",
-    city: "Los Angeles",
-    page: "1",
-    sort: "relevant",
-    type: "single-family,multi-family",
-  },
-  headers: {
-    "x-rapidapi-key": `${process.env.REACT_APP_REAL_ESTATE_KEY}`,
-    "x-rapidapi-host": "real-estate12.p.rapidapi.com",
-  },
-};
+// const options = {
+//   method: "GET",
+//   url: "https://real-estate12.p.rapidapi.com/listings/sale",
+//   params: {
+//     state: "CA",
+//     city: "Los Angeles",
+//     page: "1",
+//     sort: "relevant",
+//     type: "single-family,multi-family",
+//   },
+//   headers: {
+//     "x-rapidapi-key": `${process.env.REACT_APP_REAL_ESTATE_KEY}`,
+//     "x-rapidapi-host": "real-estate12.p.rapidapi.com",
+//   },
+// };
 
 export default () => {
-  const [properties, setProperties] = useState([]);
+  // const [properties, setProperties] = useState([]);
 
-  useEffect(() => {
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data);
-        // console.log(response.data.properties);
-        setProperties(response.data.properties);
-        // console.log(`these are the ${properties}`);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }, [])
+  // useEffect(() => {
+  //   axios
+  //     .request(options)
+  //     .then(function (response) {
+  //       console.log(response.data);
+  //       // console.log(response.data.properties);
+  //       setProperties(response.data.properties);
+  //       // console.log(`these are the ${properties}`);
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+  // }, [])
 
   return (
     <div className="real_estate">
@@ -50,13 +50,13 @@ export default () => {
         hand, offers better bargain per square foot of real estate.
       </p>
       <div>
-        <ul>
+     {/*   <ul>
           {properties.map((property, idx) => (
             <div key={idx}>
               <li>{property.permalink}</li>
             </div>
           ))}
-        </ul>
+          </ul> */}
       </div>
     </div>
   );
