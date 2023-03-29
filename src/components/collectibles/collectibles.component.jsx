d30a04095da22eb8cdd41f544739e0ea9c7f807c
Ben Hsieh <bqh5026@gmail.com>
2023-03-28 22:56:20 -0500

    Update app

--- src/components/collectibles/collectibles.component.jsx
+++ src/components/collectibles/collectibles.component.jsx
@@ -5,7 +5,7 @@ const Collectibles = () => (
   <div>
     <h1>Collectibles</h1>
     <p>
-      Collectibles can be another excellent way to invest your money.
+      Collectibles can be another excellent way to invest your money. Some examples of collectibles are baseball cards, paintings, and stamps. 
     </p>
   </div>
 );
