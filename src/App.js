e1e0f0d7a06345aae3435f789d23b9eccd9ecdc4
Ben Hsieh <bqh5026@gmail.com>
2023-11-14 16:13:52 -0600

    Update app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect} from 'react';
 import { HashRouter, Route, Link } from 'react-router-dom';
 
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
@@ -16,65 +16,69 @@ import Bonds from './components/bonds/bonds.component';
 import Investing from './images/dollar-cost-vs-lump-sum-investing-2.jpg'; 
 import Collectibles from './components/collectibles/collectibles.component';
 
-class App extends Component {
-  render() {
-    return (
-      <HashRouter basename="/">
-        <div className="App">
-          <div>
 
-            <Tabs defaultIndex={0}>
-              <TabList>
-                <Tab>
-                  <Link to="/">Home</Link>
-                </Tab>
-                 <Tab>
-                  <Link to="/bonds">Bonds</Link>
-                </Tab>
-                 <Tab>
-                  <Link to="/collectibles">Collectibles</Link>
-                </Tab>
-                <Tab>
-                  <Link to="/crypto_currencies">Crypto Currencies</Link>
-                </Tab>
-                 <Tab>
-                  <Link to="/equities">Equities</Link>
-                </Tab>
-                  <Tab>
-                  <Link to="/precious_metals">Precious Metals</Link>
-                </Tab>
-                <Tab>
-                  <Link to="/real_estate">Real Estate</Link>
-                </Tab>                            
-              </TabList>
+ 
 
-              <TabPanel>
-              {/* <h2>Any content 1</h2>*/}
-              </TabPanel>
-              <TabPanel>
-                {/* <h2>Any content 2</h2>*/}
-              </TabPanel>
-              <TabPanel></TabPanel>
-              <TabPanel></TabPanel>
-              <TabPanel></TabPanel>
-              <TabPanel></TabPanel>
-            </Tabs>
+function App() {
 
-            <Route exact path="/" component={HomePage} />
-            <Route path="/bonds" component={Bonds} />
-            <Route path="/collectibles" component={Collectibles} />
-            <Route path="/crypto_currencies" component={CryptoCurrencies} />
-            <Route path="/equities" component={Equities} />
-            <Route path="/precious_metals" component={PreciousMetals} />
-            <Route path="/real_estate" component={RealEstate} />
-          </div>
-          <div className="image">
-            <img src={Investing} alt="Investing" />
-          </div>
+  useEffect(() => {
+    fetch("http://localhost:8080/investing")
+      .then((response) => response.text())
+      .then((result) => console.log(result));
+  }, []);
+
+  return (
+    <HashRouter basename="/">
+      <div className="App">
+        <div>
+          <Tabs defaultIndex={0}>
+            <TabList>
+              <Tab>
+                <Link to="/">Home</Link>
+              </Tab>
+              <Tab>
+                <Link to="/bonds">Bonds</Link>
+              </Tab>
+              <Tab>
+                <Link to="/collectibles">Collectibles</Link>
+              </Tab>
+              <Tab>
+                <Link to="/crypto_currencies">Crypto Currencies</Link>
+              </Tab>
+              <Tab>
+                <Link to="/equities">Equities</Link>
+              </Tab>
+              <Tab>
+                <Link to="/precious_metals">Precious Metals</Link>
+              </Tab>
+              <Tab>
+                <Link to="/real_estate">Real Estate</Link>
+              </Tab>
+            </TabList>
+
+            <TabPanel>{/* <h2>Any content 1</h2>*/}</TabPanel>
+            <TabPanel>{/* <h2>Any content 2</h2>*/}</TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+          </Tabs>
+
+          <Route exact path="/" component={HomePage} />
+          <Route path="/bonds" component={Bonds} />
+          <Route path="/collectibles" component={Collectibles} />
+          <Route path="/crypto_currencies" component={CryptoCurrencies} />
+          <Route path="/equities" component={Equities} />
+          <Route path="/precious_metals" component={PreciousMetals} />
+          <Route path="/real_estate" component={RealEstate} />
         </div>
-      </HashRouter>
-    );
-  }
+        <div className="image">
+          <img src={Investing} alt="Investing" />
+        </div>
+      </div>
+    </HashRouter>
+  );
 }
 
 export default App;
